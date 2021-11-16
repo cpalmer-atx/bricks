905fa2d7c9ad4d916b22f90ad9a2224c1e98fc03
Chad Palmer <chad.n.palmer@gmail.com>
2021-11-16 17:29:01 -0600

    sanity check passing, server responds to http requests

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+// Middleware
 app.use(express.json());
+
+// Routers
+app.use('/sanityCheck', require('./routes/sanity'));
 
 module.exports = app;
