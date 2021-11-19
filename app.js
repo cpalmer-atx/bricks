bcef2c1f7b7d24bd9f7d78e2fa3dba2be9c37a9d
Chad Palmer <chad.n.palmer@gmail.com>
2021-11-19 17:17:00 -0600

    enabled morgan logger in development mode

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+const startupDebugger = require('debug')('app:startup');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+}
 // Routers
 app.use('/sanityCheck', require('./routes/sanity'));
 
