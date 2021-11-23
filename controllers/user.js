c0484f98c625b0e9d691ef58d72f83735de5697a
Chad Palmer <chad.n.palmer@gmail.com>
2021-11-23 17:42:18 -0600

    implemented user jwt validation, jwt tokens now returned when new user created

--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,53 @@
+const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
+const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 
 // @desc      Create new user
 // @route     POST /api/users/
 // @access    Private
 exports.createUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+  const { name, email, password } = req.body;
+  
   try {
-    const user = await User.create(req.body);
-    res.status(201).json({
-      success: true,
-      data: user
-    });
+    let user = await User.findOne({ email });
+    if (user) { 
+      return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+    }
+
+    const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
+
+    user = new User({ name, email, avatar, password });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+    
+    //TODO: reduce token expiration time before deploying!
+    jwt.sign(
+      payload, 
+      process.env.JWT_TOKEN, 
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+
   } catch (err) {
-    console.error(err);
-    res.status(400).json({
-      success: false,
-      msg: 'Something went wrong while creating new user.'
-    });
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
 }
 
--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,4 @@
+const { check, validationResult } = require('express-validator');
 const express = require('express');
 const { 
   createUser,
@@ -12,7 +13,11 @@ const router = express.Router();
 router
   .route('/')
   .get(getUsers)
-  .post(createUser);
+  .post([
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'A valid email is required').isEmail(),
+    check('password', 'Password with 5 or more characters required').isLength({ min: 5 })
+  ], createUser);
 
 router
   .route('/:id')
