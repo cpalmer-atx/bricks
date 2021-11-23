024001181aad6a1b9fad3be96be14afe82009e7d
Chad Palmer <chad.n.palmer@gmail.com>
2021-11-23 17:44:20 -0600

    added profile controller to be implemented next

---
+++ controllers/profile.js
@@ -0,0 +1,8 @@
+const Profile = require('../models/Profile');
+
+// @desc      Create new profile
+// @route     POST /api/profiles/
+// @access    Private
+exports.createProfile = async (req, res, next) => {
+  res.send('Coming soon!');
+}
