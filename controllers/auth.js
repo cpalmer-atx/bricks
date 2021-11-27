2c284139e715a1ec0e413cc1020a657a47a1ba88
Chad Palmer <chad.n.palmer@gmail.com>
2021-11-27 12:35:07 -0600

    added authorized user login functionality/route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const auth = require('../middleware/auth');
+const { validationResult } = require('express-validator');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 exports.getAuthorizedUser = async (req, res, next) => {
   try {
@@ -11,4 +12,45 @@ exports.getAuthorizedUser = async (req, res, next) => {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
-}
+};
+
+exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+    }
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+    }
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_TOKEN,
+      { expiresIn: process.env.TOKEN_EXP },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,19 @@
+const { check } = require('express-validator');
 const auth = require('../middleware/auth');
 const express = require('express');
 const {
-  getAuthorizedUser
+  getAuthorizedUser,
+  loginUser
 } = require('../controllers/auth');
 
 const router = express.Router();
 
 router
   .route('/')
+  .post([
+      check('email', 'A valid email is required').isEmail(),
+      check('password', 'Password is required').exists()
+    ], loginUser)
   .get(auth, getAuthorizedUser);
 
 module.exports = router;
