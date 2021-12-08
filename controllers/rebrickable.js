5269b325a98b616b2dcc9e0c7d8c983477d00142
Chad Palmer <chad.n.palmer@gmail.com>
2021-12-08 10:56:33 -0600

    add model now  retrieves/parses data from rebrickable

--- controllers/rebrickable.js
+++ controllers/rebrickable.js
@@ -1,29 +1,53 @@
+const Model = require("../models/Model");
 const User = require("../models/User");
 const axios = require('axios');
 
 exports.addModel = async (req, res, next) => {
   const { user_id, model_num } = req.params;
-  const user = await User.findById(user_id);
-  const URI = `https://rebrickable.com/api/v3/lego/sets/${model_num}`;
-  console.log(`${process.env.BRICKS_KEY}`);
+  const setURI = `https://rebrickable.com/api/v3/lego/sets/${model_num}`;
+  const partsURI = `${setURI}/parts`;
   const headers = {
     'Authorization': `key ${process.env.BRICKS_KEY}`
   }
-  
+
+  const user = await User.findById(user_id);
   if (!user) {
     return res.status(400).json({ msg: 'No user found with provided user ID' });
   }
-
+  
   try {
-    const model = await axios.get(URI, { headers });
-    
+    const model = await axios.get(setURI, { headers });
+    const parts = await axios.get(partsURI, { headers });
+
+    const inv_array = parts.data.results.map(part => 
+      {
+        const newPart = {
+          part_number: part.part.part_num,
+          part_img: part.part.part_img_url,
+          quantity: part.quantity,
+          part_color: {
+            name: part.color.name,
+            rgb: part.color.rgb,
+            transparent: part.color.is_trans
+          }
+        }
+        return newPart;
+      });
+
+    const payload = {
+      model_name: model.data.name,
+      model_number: model.data.set_num,
+      brick_count: model.data.num_parts,
+      model_img: model.data.set_img_url,
+      quantity: 1,
+      inventory: inv_array
+    }
+
+    const new_model = new Model();
+
     res.status(200).send({ 
-      // msg: 'pinged addModel successfully!',
-      // params: req.params,
-      // query: req.query,
-      // user: user,
-      // key: headers.Authorization,
-      data: model
+      msg: 'pinged addModel successfully!',
+      data: payload
     });
 
   } catch (err) {
