791b1d67dc41209b11692546e99a7bccd38d8a59
Chad Palmer <chad.n.palmer@gmail.com>
2021-12-17 20:14:37 -0600

    improved addModel, added deleteModel

--- controllers/rebrickable.js
+++ controllers/rebrickable.js
@@ -1,21 +1,29 @@
-const Model = require("../models/Model");
 const User = require("../models/User");
 const axios = require('axios');
 
 exports.addModel = async (req, res, next) => {
-  const { user_id, model_num } = req.params;
+  const { model_num } = req.params;
   const setURI = `https://rebrickable.com/api/v3/lego/sets/${model_num}`;
   const partsURI = `${setURI}/parts`;
   const headers = {
     'Authorization': `key ${process.env.BRICKS_KEY}`
   }
 
-  const user = await User.findById(user_id);
-  if (!user) {
-    return res.status(400).json({ msg: 'No user found with provided user ID' });
-  }
   
   try {
+    const user = await User.findById(req.user.id).select('-password');
+    if (!user) {
+      return res.status(400).json({ msg: 'No user found with provided user ID' });
+    }
+
+    const model_exists = user.model_inventory.find(lego => lego.model_number === model_num);
+
+    if (model_exists) {
+      ++model_exists.quantity;
+      user.save();
+      return res.status(200).send({ msg: `Model ${model_num} quantity updated in user profile.`});
+    }
+
     const model = await axios.get(setURI, { headers });
     const parts = await axios.get(partsURI, { headers });
 
@@ -33,12 +41,14 @@ exports.addModel = async (req, res, next) => {
         }
         return newPart;
       });
+    
+    const { name, set_num, num_parts, set_img_url } = model.data;
 
     const payload = {
-      model_name: model.data.name,
-      model_number: model.data.set_num,
-      brick_count: model.data.num_parts,
-      model_img: model.data.set_img_url,
+      model_name: name,
+      model_number: set_num,
+      brick_count: num_parts,
+      model_img: set_img_url,
       quantity: 1,
       parts: inv_array
     }
@@ -52,7 +62,30 @@ exports.addModel = async (req, res, next) => {
     });
 
   } catch (err) {
-    console.error(err);
+    console.error(err.message);
+    res.status(500).send(err.message);
+  }
+};
+
+exports.deleteModel = async (req, res, next) => {
+  const { model_num } = req.params;
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    if (!user) {
+      return res.status(400).json({ msg: 'No user found with provided user ID' });
+    }
+
+    const existing = user.model_inventory.find(lego => lego.model_number === model_num);
+
+    if (existing) {
+      existing.quantity > 0 ? --existing.quantity : existing.quantity; 
+      user.save();
+      return res.status(200).send({ msg: `Model ${model_num} quantity updated in user profile.`});
+    }
+    return res.status(400).send({ msg: `Model ${model_num} not found in user inventory.` });
+
+  } catch (err) {
+    console.error(err.message);
     res.status(500).send('Server Error');
   }
 };
--- routes/rebrickable.js
+++ routes/rebrickable.js
@@ -1,12 +1,15 @@
+const auth = require('../middleware/auth');
 const express = require('express');
 const {
-  addModel
+  addModel,
+  deleteModel
 } = require('../controllers/rebrickable');
 
 const router = express.Router();
 
 router
-  .route('/:user_id/:model_num')
-  .post(addModel);
+  .route('/:model_num')
+  .post(auth, addModel)
+  .delete(auth, deleteModel);
 
   module.exports = router;
